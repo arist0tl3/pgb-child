0cc0a3b0237128e7e1353fcebf0f48848c6a454c
Sean Elliot <sean@vigorwebsolutions.com>
2015-06-29 12:55:46 -0700

    Latest update

--- functions.php
+++ functions.php
@@ -7,7 +7,6 @@
 add_action( 'wp_enqueue_scripts', 'pgb_child_enqueue_styles' );
 function pgb_child_enqueue_styles() {
     wp_enqueue_style( 'pgb', get_template_directory_uri() . '/style.css' );
-<<<<<<< HEAD
 }
 
 add_filter( 'home_url', 'swap_url' );
@@ -16,6 +15,3 @@ function swap_url( $url )
 {
     return "http://ninthlink.com";
 }
-=======
-}
->>>>>>> 710107380f68896f9a91dca60cc91fc92fa32dc0
